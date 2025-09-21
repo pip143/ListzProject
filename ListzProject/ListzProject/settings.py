da8856c1cea73af1caec9a2d9655df5edf66f6e3
pip143 <ninetailedbeast009@gmail.com>
2025-09-21 23:41:45 +0800

    feature(Listz): setup templates

--- ListzProject/ListzProject/settings.py
+++ ListzProject/ListzProject/settings.py
@@ -54,10 +54,11 @@ ROOT_URLCONF = 'ListzProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],  # ✅ add this
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -65,6 +66,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'ListzProject.wsgi.application'
 
